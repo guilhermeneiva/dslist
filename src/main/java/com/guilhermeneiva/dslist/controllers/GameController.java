2b8295374f9dda102c6944754337bc72218aeef5
Guilherme Neiva <guineiva51@gmail.com>
2024-11-11 19:03:29 -0300

    Game repository, service, controller

---
+++ src/main/java/com/guilhermeneiva/dslist/controllers/GameController.java
@@ -0,0 +1,26 @@
+package com.guilhermeneiva.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.guilhermeneiva.dslist.dto.GameMinDTO;
+import com.guilhermeneiva.dslist.entities.Game;
+import com.guilhermeneiva.dslist.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+	@Autowired
+	private GameService gameService;
+
+	@GetMapping
+	public List<GameMinDTO> findAll() {
+		List<GameMinDTO> result = gameService.findAll();
+		return result;
+	}
+}
